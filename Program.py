968b04187b8415ae9c274e6f454f9ea236d7bbde
David James Button <davidb@pageuppeople.com>
2017-01-22 19:31:21 +1100

    Initial commit

---
+++ Program.py
@@ -0,0 +1,266 @@
+import bs4
+import datetime
+import math
+import logging
+import os.path
+import random
+import requests
+import time
+from fake_useragent import UserAgent
+
+# Setup logging for console display
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.INFO
+)
+
+# Setup global variables
+MIN_WAIT = 10
+MAX_WAIT = 15
+INPUT_FILENAME = 'Queries.txt'
+OUTPUT_FILENAME = 'Results.csv'
+N_RESULTS = 50
+MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+DATE_NOW = datetime.datetime.now().date()
+BASE_URL = "https://www.google.com/search"
+
+
+##### High-level search-related methods #####
+
+
+def safely_search_and_save(n, queries, date):
+    """Safely searches google for the first n results for each query
+    using the specified date in the search. Results are saved afterwards."""
+    for query in queries:
+        search_and_save(n, query, date)
+        logging.info("Waiting a few seconds...")
+        logging.info("")
+        wait_random()
+
+
+def search_and_save(n, query, date):
+    """Searches google for the first n results for 'RFP "{query}" {date}'.
+    Results are saved afterwards."""
+    queried_on = DATE_NOW
+    search_term = get_search_string(query, date)
+    results = get_first_n_results(n, search_term)
+    full_results = map(lambda r :
+        (search_term, query, date.strftime("%Y %m"), queried_on.strftime("%Y %m"), r[0], r[1], checked_url(r[2])),
+        results)
+    logging.info("Saving results")
+    append_results(full_results)
+
+
+def get_first_n_results(n, search_term):
+    """Searches google for the first n results for a particular search term and returns
+    a list of (rank, title, url)."""
+    safe_n = get_safe_n(n)
+    parameters = get_params(safe_n, search_term)
+    logging.info("Searching for : %s" % search_term)
+    html = search(BASE_URL, parameters)
+    parser = bs4.BeautifulSoup(html, "html.parser")
+    results = list()
+    item_containers = parser.select(".g")
+    for c in item_containers:
+        data = c.select("h3.r")
+        if (len(data)) == 0:
+            continue
+        item = data[0]
+        heading_data = item.select("a")[0]
+        title = heading_data.getText()
+        raw_url = heading_data.get("href")
+        url = clean_url(raw_url)
+        if title and url:
+            results.append((len(results)+1, title, url))
+        if len(results) == n:
+            break
+    return results
+
+
+def search(url, params=None):
+    """Searches a url with given query-string parameters and returns the html."""
+    full_url = url
+    if params is not None:
+        param_key_values = list(params.items())
+        query_string = "".join(map(lambda p: "&%s=%s" % p, param_key_values))
+        if len(query_string) != 0:
+            query_string = "?" + query_string
+        full_url = url + query_string
+    logging.debug("full url : %s" % full_url)
+    response = requests.get(full_url, headers=get_random_headers())
+    response.raise_for_status()
+    return response.text
+
+
+##### Low-level user-logic functions #####
+
+
+def get_random_headers():
+    """Gets random headers to query google with."""
+    headers = {
+        "User-Agent": UserAgent().ie
+    }
+    logging.debug(headers)
+    return headers
+
+
+def checked_url(string):
+    """Checks for valid URL and returns result."""
+    if "," in string:
+        return "ERROR"
+    if len(string) > 255:
+        return "ERROR"
+    return string
+
+
+def clean_url(href):
+    """Cleans a href found from the result item heading."""
+    if href.startswith("/search"):
+        return None
+    start = href.find("http")
+    href = href[start:]  # remove preceding '/url?q=' , etc
+    param_start = href.find("&sa")
+    href = href[:param_start]  # remove proceeding parameters
+    return href
+
+
+def get_safe_n(n):
+    """Inflates the number of results shown as some returned are 'improprer'."""
+    if n < 10:
+        n += 5
+    n = math.ceil(1.2 * n)
+    return n
+
+
+def get_params(count, query):
+    """Gets the google query-string parameters."""
+    params = {
+        "num": count,   # results per page
+        "q": query      # search term
+    }
+    return params
+
+
+def append_results(results):
+    """Appends the results to the results file."""
+    if not os.path.isfile(OUTPUT_FILENAME):
+        create_results_file()
+    file = open_when_free(OUTPUT_FILENAME, 'a')
+    for result in results:
+        logging.debug(result)
+        file.write('%s,%s,%s,%s,%s,"%s",=HYPERLINK("%s")\n' % result)
+    file.close()
+
+
+def create_results_file():
+    """Creates new results file with headers."""
+    file = open_when_free(OUTPUT_FILENAME, 'w')
+    file.write("QueryTotal,QueryRFP,QueryDate,QueriedOn,Rank,Title,Url\n")
+    file.close()
+
+
+def read_queries():
+    """Reads the strings from the input file and returns them."""
+    with open(INPUT_FILENAME) as file:
+        queries = [line.strip() for line in file.readlines()]
+    return queries
+
+
+def get_search_string(query, date):
+    """Given a query and date, this returns a string 'RFP "{query}" {date}'."""
+    return 'RFP "%s" %s' % (query, date.strftime("%B %Y"))
+
+
+def get_date():
+    """Gets the date from the user either using today's date or user-defined."""
+    print("How would you like to choose a date?")
+    mode = get_int_input_in_range("Current date (1) or Manual selection (2)?", 1, 2)
+    if mode == 1:
+        return DATE_NOW
+    elif mode == 2:
+        months_ind_val = list(enumerate(MONTHS, start=1))
+        month_str_lines = map(lambda x: "(%2s) %s" % (x[0], x[1]), months_ind_val)
+        months_disp = "\n".join(month_str_lines)
+        month = get_int_input_in_range("Choose a month:\n" + months_disp, 1, 12)
+        year = get_int_input("Enter a year:")
+        return datetime.date(year, month, 1)
+
+
+##### Utility Functions #####
+
+
+def get_int_input_in_range(prompt, min, max):
+    """Requests a integer input between min and max."""
+    print(prompt)
+    while True:
+        answer = get_int_input()
+        if not (min <= answer <= max):
+            print("Error: Digit not in range")
+            continue
+        return answer
+
+
+def get_int_input(prompt=None):
+    """Requests an integer input."""
+    if prompt is not None:
+        print(prompt)
+    while True:
+        answer = input("> ")
+        if not answer.isdigit():
+            print("Error: Not a digit")
+            continue
+        return int(answer)
+
+
+def wait_random():
+    """Waits a random time between min and max."""
+    wait_time = random.randrange(MIN_WAIT, MAX_WAIT)
+    logging.debug("Waiting %s seconds" % wait_time)
+    time.sleep(wait_time)
+
+
+def open_when_free(filename, mode):
+    """Opens file for writing and waits for the resource to be freed. The file handler is returned."""
+    while True:
+        try:
+            file = open(filename, mode, encoding='utf-8')
+            return file
+        except PermissionError:
+            logging.info("Error: Cannot open %s as it is in use by another process. Please close the file." % filename)
+            logging.info("Please close the file and press ENTER when you have done so.")
+            input("")
+
+
+#### MAIN STARTER #####
+
+if __name__ == "__main__":
+
+    # Introduction
+    print("Google Automated Search")
+    print("By David Button")
+    print("For Ian")
+    print("")
+
+    # Get manual or automatic date
+    date = get_date()
+
+    # Read rfp queries from file
+    rfp_queries = read_queries()
+    logging.info("Found %s queries\n" % len(rfp_queries))
+    logging.debug(rfp_queries)
+
+    # Safely search and save the results
+    safely_search_and_save(N_RESULTS, rfp_queries, date)
---
+++ README.txt
@@ -0,0 +1,36 @@
+////////////////////////
+
+Google Automated Search
+
+////////////////////////
+
+DESCRIPTION
+
+This program will search google for a set of RFP terms 
+and store the first n results in an excel.
+
+TECHNICAL REQUIREMENTS
+
+1. Install Python 3.X (This was developed in 3.6)
+2. Run 'pip install requests' in CMD
+3. Run 'pip install beautifulsoup4' in CMD
+3. Run 'pip install fake-useragent' in CMD
+(pip should work after Python is installed. Otherwise, search google how to install pip-installer)
+
+SETUP REQUIREMENTS
+
+1. Setup Queries.txt with a list of queries to search for. List each query on a new line. For example, if you have 'Legislation' on one line, Google will search for 'RFP "Legislation" {date}".
+2. Check the global variables in Program.py for any changes. I'd say they're okay as they are.
+
+HOW TO RUN
+
+1. Open Python IDLE
+2. Choose File -> Open and locate Program.py
+3. Choose Run -> Run Module
+4. Follow any program instructions
+
+COMMENTS
+
+Let me know if you need any help or if anything doesn't work! I hope this serves you well.
+
+
